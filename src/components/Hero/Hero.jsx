db2a2b23267b657ddacff40e804dbf89af44cbbd
Renato <u201511892@upc.edu.pe>
2023-04-19 22:49:27 -0500

    feat: hero jsx css

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,11 @@
 import React, { useContext } from "react";
 import "./Hero.css";
 import blob2 from "../../img/blob1.svg";
-import thumbup from "../../img/thumbup.png";
-import crown from "../../img/crown.png";
-import FloatinDiv from "../FloatingDiv/FloatingDiv";
 import { themeContext } from "../../Context";
-import { motion } from "framer-motion";
 import { Link } from "react-scroll";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faInstagram, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
-//import { faGitHub } from '@fortawesome/free-solid-svg-icons'
+import me from "../../img/me.png";
 
 const Hero = () => {
   // context
@@ -18,27 +14,27 @@ const Hero = () => {
 
   return (
     <div className="Hero" id="Intro">
-
       <div className="img-container">
         <img src={blob2} alt="" />
       </div>
 
-      <div className="i-left">
-        <div className="i-name">
+      <div className="wrapper">
+        <div className="profile">
           <span style={{ color: darkMode ? "white" : "" }}>Hi! I Am</span>
           <span>Renato Castillo</span>
           <span>
-            Fullstack Developer with high level of experience in web designing
+            <span style={{ fontWeight: "bold" }}>Fullstack Developer</span> with high level in web designing
             and back-end development, always focusing in quality of the service.
           </span>
-        </div>
-        <Link to="contact" smooth={true} spy={true}>
-          <button className="button i-button">Hire me</button>
-        </Link>
-        <div className="i-icons">
-          <FontAwesomeIcon icon={faGithub} size="6x" style={{ color: "#FB5635", fontSize: "5rem" }} />
-          <FontAwesomeIcon icon={faLinkedinIn} size="6x" style={{ color: "#FB5635", fontSize: "5rem" }} />
-          <FontAwesomeIcon icon={faInstagram} size="6x" style={{ color: "#FB5635", fontSize: "5rem" }} />
+          <Link to="contact" spy={true} smooth={true}>
+            <button className="button n-button">Contact</button>
+          </Link>
+          <img src={me} alt="" />
+          <div className="icons">
+            <FontAwesomeIcon icon={faGithub} size="6x" style={{ color: "#FB5635", fontSize: "5rem" }} />
+            <FontAwesomeIcon icon={faLinkedinIn} size="6x" style={{ color: "#FB5635", fontSize: "5rem" }} />
+            <FontAwesomeIcon icon={faInstagram} size="6x" style={{ color: "#FB5635", fontSize: "5rem" }} />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Toggle from "../Toggle/Toggle";
-import me from "../../img/me.png";
 import "./Navbar.css";
 import { Link } from "react-scroll";
 import { FaBars } from "react-icons/fa";
@@ -19,12 +18,6 @@ const Navbar = () => {
     <div className="n-wrapper" id="Navbar">
       {/* left */}
       <div className="n-left">
-        <div className="n-name">Renato</div>
-        <img src={me} alt="" style={{ width: "80px" }} />
-        <Toggle />
-      </div>
-      {/* right */}
-      <div className="n-right">
         <div className="n-list">
           <ul style={{ listStyleType: "none" }}>
             <li>
@@ -42,22 +35,21 @@ const Navbar = () => {
                 Work
               </Link>
             </li>
-            <li>
-              <Link to="Contact" spy={true} smooth={true}>
-                Contact
-              </Link>
-            </li>
+
           </ul>
         </div>
-        <Link to="contact" spy={true} smooth={true}>
-          <button className="button n-button">Contact</button>
-        </Link>
 
         <button
           className="nav-btn"
           onClick={showNavbar}>
           <FaBars />
         </button>
+      </div>
+
+      {/* right */}
+      <div className="n-right">
+
+        <Toggle />
       </div>
     </div>
   );
