c5769df658acd821bb43c40d7cd7371f469598f4
Renato <u201511892@upc.edu.pe>
2023-04-29 15:22:11 -0500

    feat: skills, my work and footer section with brand colors

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import "./Footer.css";
-import Wave from "../../img/wave.png";
-import Insta from "@iconscout/react-unicons/icons/uil-instagram";
-import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
 import Gitub from "@iconscout/react-unicons/icons/uil-github";
+import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <img src={Wave} alt="" style={{ width: "100%" }} />
       <div className="f-content">
-        <span>Zainkeepscode@gmail.com</span>
         <div className="f-icons">
-          <Insta color="white" size={"3rem"} />
-          <Facebook color="white" size={"3rem"} />
           <Gitub color="white" size={"3rem"} />
+          <FaLinkedin color="white" size={"3rem"} />
         </div>
       </div>
     </div>
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,26 +1,11 @@
 import React, { useContext } from "react";
 import "./Skills.css";
-import Card from "../Card/Card";
-import HeartEmoji from "../../img/heartemoji.png";
-import Glasses from "../../img/glasses.png";
-import Humble from "../../img/humble.png";
 import { themeContext } from "../../Context";
-import { motion } from "framer-motion";
-import Resume from './resume.pdf';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faReact, faHtml5, faCss3, faJs, faCss3Alt } from "@fortawesome/free-brands-svg-icons";
-import { FaJs } from "react-icons/fa";
 
 const Skills = () => {
   // context
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
-
-  // transition
-  const transition = {
-    duration: 1,
-    type: "spring",
-  };
 
   return (
     <div className="skills">
@@ -38,35 +23,35 @@ const Skills = () => {
       </div>
       <div className="icons" >
         <div>
-          <a href="https://cdnlogo.com/logo/react_22568.html"><img src="https://cdn.cdnlogo.com/logos/r/63/react.svg" alt="React" /></a>
+          <a href="https://react.dev/"><img src="https://cdn.cdnlogo.com/logos/r/63/react.svg" alt="React" /></a>
           <span>React</span>
         </div>
         <div className="Net">
-          <a href=""><img src="https://upload.wikimedia.org/wikipedia/commons/7/7d/Microsoft_.NET_logo.svg" alt=".NET" /></a>
+          <a href="https://dotnet.microsoft.com/en-us/"><img src="https://upload.wikimedia.org/wikipedia/commons/7/7d/Microsoft_.NET_logo.svg" alt=".NET" /></a>
           <span>.NET</span>
         </div>
         <div>
-          <a href="https://cdnlogo.com/logo/javascript_36689.html"><img src="https://cdn.cdnlogo.com/logos/j/33/javascript.svg" alt="JavaScript" /></a>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"><img src="https://cdn.cdnlogo.com/logos/j/33/javascript.svg" alt="JavaScript" /></a>
           <span>Javascript</span>
         </div>
         <div>
-          <a href="https://cdnlogo.com/logo/csharp_39895.html"><img src="https://cdn.cdnlogo.com/logos/c/70/csharp.svg" alt="Csharp" /></a>
+          <a href="https://learn.microsoft.com/en-us/dotnet/csharp/"><img src="https://cdn.cdnlogo.com/logos/c/70/csharp.svg" alt="Csharp" /></a>
           <span>Csharp</span>
         </div>
         <div>
-          <a href="https://cdnlogo.com/logo/html_50218.html"><img src="https://cdn.cdnlogo.com/logos/h/84/html.svg" alt="HTML" /></a>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/HTML"><img src="https://cdn.cdnlogo.com/logos/h/84/html.svg" alt="HTML" /></a>
           <span>HTML</span>
         </div>
         <div>
-          <a href="https://cdnlogo.com/logo/postgresql_39744.html"><img src="https://cdn.cdnlogo.com/logos/p/93/postgresql.svg" alt="PostgreSQL" /></a>
+          <a href="https://www.postgresql.org/"><img src="https://cdn.cdnlogo.com/logos/p/93/postgresql.svg" alt="PostgreSQL" /></a>
           <span>PostgreSQL</span>
         </div>
         <div>
-          <a href="https://cdnlogo.com/logo/css_36679.html"><img src="https://cdn.cdnlogo.com/logos/c/18/css.svg" alt="CSS" /></a>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/CSS"><img src="https://cdn.cdnlogo.com/logos/c/18/css.svg" alt="CSS" /></a>
           <span>CSS</span>
         </div>
         <div>
-          <a href="https://cdnlogo.com/logo/azure_788.html"><img src="https://cdn.cdnlogo.com/logos/a/12/azure.svg" /></a>
+          <a href="https://azure.microsoft.com/en-us"><img src="https://cdn.cdnlogo.com/logos/a/12/azure.svg" /></a>
           <span>Azure</span>
         </div>
       </div>
