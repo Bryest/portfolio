9889e734f3d4457a011d615b8ff1c6bbcf85a055
Renato <u201511892@upc.edu.pe>
2023-04-29 15:22:11 -0500

    feat: skills, my work and footer section with brand colors

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Skills from "./components/Skills/Skills";
 import "./App.css";
-import Experience from "./components/Experience/Experience";
 import Works from "./components/Works/Works";
-import Portfolio from "./components/Portfolio/Portfolio";
-import Testimonial from "./components/Testimonials/Testimonial";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import { useContext, useEffect } from "react";
@@ -19,23 +16,21 @@ function App() {
   })
 
   return (
-    <div
-      className="App"
-      style={{
-        background: darkMode ? "var(--background-dark-color)" : "",
-        color: darkMode ? "var(--background-light-color)" : "",
-      }}
-    >
-      <Navbar />
-      <Hero />
-      <Skills />
-      {/*<Work />*/}
-      {/*<Works />*/}
-      {/*<Portfolio />*/}
-      {/*<Testimonial />*/}
-      {/*<Contact />*/}
-      {/*<Footer />*/}
-    </div>
+    <>
+      <div
+        className="App"
+        style={{
+          background: darkMode ? "var(--background-dark-color)" : "",
+          color: darkMode ? "var(--background-light-color)" : "",
+        }}
+      >
+        <Navbar />
+        <Hero />
+        <Skills />
+        <Works />
+      </div>
+      <Footer />
+    </>
   );
 }
 
